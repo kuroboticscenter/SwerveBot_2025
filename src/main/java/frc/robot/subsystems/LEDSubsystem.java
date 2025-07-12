48f17c7ba07e9245109ba2a1e6ca979c80a06271
Kettering Robotics Center <roboticscenter@kettering.edu>
2025-07-12 18:51:14 -0400

    Update LEDSubsystem

--- src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -4,6 +4,7 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.configs.LEDConfigs;
 import com.ctre.phoenix6.controls.SolidColor;
 import com.ctre.phoenix6.controls.StrobeAnimation;
 import com.ctre.phoenix6.hardware.CANdle;
@@ -13,9 +14,15 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.util.Color;
 
 public class LEDSubsystem extends SubsystemBase {
-  CANdle LED1;
-  CANdle LED2;
+  CANdle LED1 = new CANdle(51, "rio");
+  CANdle LED2 = new CANdle(52, "rio");
+  
+  // Colors
   RGBWColor rslOrange = new RGBWColor(new Color(200, 25, 0));
+  RGBWColor white = new RGBWColor(Color.kWhite);
+  RGBWColor black = new RGBWColor(Color.kBlack);
+  
+  // Controls
   SolidColor rslDisabled = new SolidColor(0, 7).withColor(rslOrange);
   StrobeAnimation rslEnabled = new StrobeAnimation(0, 7)
     .withColor(rslOrange)
@@ -23,11 +30,14 @@ public class LEDSubsystem extends SubsystemBase {
     
   /** Creates a new LEDSubsystem. */
   public LEDSubsystem() {
-    LED1 = new CANdle(51);
-    LED2 = new CANdle(52);
+    LEDConfigs ledConfigs = new LEDConfigs().withBrightnessScalar(.1);
 
-    LED1.setControl(rslDisabled);
-    LED2.setControl(rslEnabled);
+    LED1.getConfigurator().apply(ledConfigs);
+    LED2.getConfigurator().apply(ledConfigs);
+  
+    LED1.setControl(new SolidColor(0, 7).withColor(black));
+    LED2.setControl(new SolidColor(0, 7).withColor(black));
+
   }
 
   @Override
